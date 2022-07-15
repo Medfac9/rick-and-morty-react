ab5945a4e97530834dfe7503f555470ceb0a73d1
Rafa Medina <rafael.med@celtiberian.es>
2022-07-15 12:02:10 +0200

    refactoring

--- src/features/allCharacters/allCharactersSlice.js
+++ src/features/allCharacters/allCharactersSlice.js
@@ -7,26 +7,33 @@ export const loadCharacters = createAsyncThunk(
     async (filters={}) => {
         let url = '';
 
+        // Si me viene una url, directamente pido la petición  dicha url
         if(filters.url){
             url = filters.url;
         }
+        // Sino calculo yo la url
         else{
             url = 'https://rickandmortyapi.com/api/character/';
+            // Para iniciar los parametros, si no se mete el primer query cambiara a ?
             let symbol = '&';
     
+            // Si me viene un nombre es lo primero que se añade a la petición de la api
             if(filters.name){
                 url += '?name=' + filters.name;
             }
             else{
                 symbol = '?';
             }
-    
+            
+            // Filtro por los filtros que me vengan
             if(filters.filters){
                 let url_filters = '';
                 let status = '';
                 let gender = '';
-    
+                
+                // Si viene algo distinto a all filtro por el
                 if(filters.filters['status'] !== 'status-all'){
+                    // Si viene con status- es para diferenciarlo del gender-unknown
                     if(filters.filters['status'] === 'status-unknown'){
                         status = 'unknown'
                     }
@@ -73,9 +80,7 @@ const sliceOptions = {
         info: [],
         isLoading: false,
     },
-    reducers: {
-        
-    },
+    reducers: {},
     extraReducers: {
         [loadCharacters.pending]: (state, action) => {
             state.isLoading = true;
